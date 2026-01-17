1bb777d86b231431408700b5fdd7283e5c6ea68b
Baso-CODE <basoafrizamuhafriza@gmail.com>
2026-01-17 22:23:14 +0700

    feat: fix navbar bug

--- app/components/navigation/Navbar.tsx
+++ app/components/navigation/Navbar.tsx
@@ -35,10 +35,10 @@ export default function Navbar() {
   return (
     <header
       className={cn(
-        "fixed top-0 left-0 right-0 z-50 transition-all duration-300 ease-in-out",
+        "fixed top-0 left-0 right-0 z-50 transition-all duration-300 ease-in-out overflow-x-hidden",
         isScrolled
-          ? "bg-white/80 backdrop-blur-md shadow-sm py-4" // Style saat discroll (Putih transparan + Blur + Shadow)
-          : "bg-transparent py-6" // Style awal (Transparan & lebih renggang)
+          ? "bg-white/80 backdrop-blur-md shadow-sm py-4"
+          : "bg-transparent py-6"
       )}>
       <div className="container mx-auto px-4 md:px-8">
         <div className="grid grid-cols-3 items-center">
